c686de03714bd5287f04534f0cdac79b15db5b18
staticdeng <1301829682@qq.com>
2021-03-21 22:05:01 +0800

    使用Rollup搭建开发环境

---
+++ vue-principle-2020/vue-principle/rollup.config.js
@@ -0,0 +1,26 @@
+import babel from 'rollup-plugin-babel';
+import serve from 'rollup-plugin-serve';
+
+export default {
+  // 定义入口文件
+  input: './src/index.js',
+  output: {
+    format: 'umd', // 定义模块化类型(支持esModule commonjs umd)
+    name: 'Vue', // 打包后全局变量的名字
+    file: 'dist/umd/vue.js', // 输出文件名
+    sourcemap: true, // 转换后代码和转换前代码作映射表，可以调试转换后的代码
+  },
+  // 插件
+  plugin: [
+    // babel编译
+    babel({
+      exclude: 'node_modules/**', // 排除目录
+    }),
+    // 配置的服务器
+    serve({
+      port: 3000,
+      contentBase: '',
+      openPage: '/index.html'
+    })
+  ]
+}
